1e99e9d68eecb29a12f80f6e4d080efb0a476708
Quang toan TRAN <qtran@e2r7p3.42.fr>
2019-06-25 11:53:04 +0200

    updating ft_lib_lem_in.h

---
+++ include/ft_lib_lem_in.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lib_lem_in.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qtran <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/25 11:24:47 by qtran             #+#    #+#             */
+/*   Updated: 2019/06/25 11:24:48 by qtran            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIB_LEM_IN_H
+# define FT_LIB_LEM_IN_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <string.h>
+# include "../libft/includes/libft.h"
+# include "../libft/includes/get_next_line.h"
+# define STDIN_DEFAULT 0
+
+typedef struct s_data
+{
+  int level;
+} t_data;
+
+typedef struct		s_room
+{
+	char			*name;
+  int       ant_cur; // index of ant / -1
+  int       type; //start / end / middle
+	int				nb_links;
+	t_list			*links;
+	int				flag;
+  t_data    *data;
+}					t_room;
+
+typedef struct		s_global
+{
+	t_room			*start;
+	t_room			*end;
+	int				nb_room;
+	int				nb_ant;
+	char			**path;
+	char			**working_path;
+	t_room			**rooms;
+}					t_global;
+
+#endif
---
+++ libft/sources/ft_lstaddback.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstaddback.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qtran <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/25 11:35:28 by qtran             #+#    #+#             */
+/*   Updated: 2019/06/25 11:35:30 by qtran            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstaddback(t_list **alst, t_list *new)
+{
+	t_list *ptr;
+
+	if (alst && *alst)
+	{
+		ptr = *alst;
+		while (ptr->next)
+			ptr = ptr->next;
+		ptr->next = new;
+	}
+	else if (alst)
+		*alst = new;
+}
