cc23110b6630a715b88a8c50b161d10b4516b3a0
Goki LY <gly@student.42.fr>
2019-07-02 09:28:44 +0200

    Finish the first version of path finding algorithm.

--- src/ft_explore_backward.c
+++ src/ft_explore_backward.c
@@ -6,7 +6,7 @@
 /*   By: gly <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 11:33:55 by gly               #+#    #+#             */
-/*   Updated: 2019/06/27 15:58:31 by gly              ###   ########.fr       */
+/*   Updated: 2019/07/02 09:20:06 by gly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 #include "ft_find_solution.h"
 
 static inline void	ft_add_inverse_flow(t_global *glob,
-		t_room *from, t_room *to)
+		t_room *from, t_room *to, int rank)
 {
 	glob->working_path[from->i][to->i] |= NEGATIVE;
 	glob->r_status[from->i] |= VISITED;
 }
 
 int					ft_explore_backward(t_global *glob,
-		t_roomlst **room_lst, t_room *room)
+		t_roomlst **room_lst, t_room *room, int rank)
 {
 	t_roomlst	*next_room;
 	t_roomlst	*elem;
 	int			ret;
 
+	if (glob->r_status[room->from->i] & VISITED || room->from == glob->start)
+		return (0);
 	ft_add_inverse_flow(glob, room, room->from);
-	if (room->from == glob->start)
-		return (1);
 	next_room = room->from->links;
 	while (next_room != NULL)
 	{
@@ -37,11 +37,11 @@ int					ft_explore_backward(t_global *glob,
 				!(glob->r_status[next_room->r->i] & VISITED))
 		{
 			ft_add_room_to_path(glob, room->from, next_room->r);
-			if (!(elem = ft_roomlst_new(next_room)))
+			if (!(elem = ft_roomlst_new(next_room, rank)))
 				return (-1);
-			ft_roomlst_add_second(room_lst, elem);
+			ft_roomlst_add_rank(room_lst, elem);
 		}
 		next_room = next_room->next;
 	}
-	return (ft_explore_backward(glob, room_lst, from));
+	return (ft_explore_backward(glob, room_lst, from, rank + 1));
 }
--- src/ft_find_path.c
+++ src/ft_find_path.c
@@ -6,7 +6,7 @@
 /*   By: gly <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 12:41:42 by gly               #+#    #+#             */
-/*   Updated: 2019/06/27 16:05:38 by gly              ###   ########.fr       */
+/*   Updated: 2019/07/02 09:27:40 by gly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,8 @@ static inline int	ft_explore_next_room(t_global *glob, t_roomlst *next_room,
 		{
 			if (glob->r_status[next_room->r->i] & ACTIVE)
 			{
-				if (ft_explore_backward(glob, room_lst, next_room->r))
+				if (ft_explore_backward(glob, room_lst, next_room->r,
+							room_lst->rank + 1))
 					return (-1);
 			}
 			else
@@ -39,9 +40,10 @@ static inline int	ft_explore_next_room(t_global *glob, t_roomlst *next_room,
 				ft_add_room_to_path(glob, (*room_lst)->r, next_room->r);
 				if (next_room->r == glob->end)
 					return (0);
-				else if (!(elem = ft_lstnew(next_room->r)))
+				else if (!(elem = ft_lstnew(next_room->r,
+								room_lst->rank == 0 ? 0 : room_lst->rank - 1)))
 					return (-1);
-				ft_lstpush(room_lst, elem);
+				ft_roomlst_add_rank(room_lst, elem);
 			}
 		}
 		next_room = next_room->next;
