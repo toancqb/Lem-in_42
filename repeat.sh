7fcc06ca168e089f17c97de01b808617f77b7cb4
Goki LY <gly@student.42.fr>
2019-07-11 13:42:58 +0200

    Upgrade the repeat.sh script to take all options.

--- repeat.sh
+++ repeat.sh
@@ -3,11 +3,35 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+echo "Option for generator? (Choose from 1-5)"
+echo -e "1. --flow-one\n2. --flow-ten\n3. --flow-thousand\n4. --big\n5. --big-superposition."
+read optn
+case $optn in
+	1)
+		opt="--flow-one"
+		;;
+	2)
+		opt="--flow-ten"
+		;;
+	3)
+		opt="--flow-thousand"
+		;;
+	4)
+		opt="--big"
+		;;
+	5)
+		opt="--big-superposition"
+		;;
+	*)
+		echo "Invalid argument"
+		;;
+esac
+
 echo "Number of repeat"
 read n
 for i in $(seq 1 $n)
 do
-	./../generator --big-superposition > tmp.map
+	./../generator $opt > tmp.map
 	exp="$(tail -n 1 tmp.map | rev | cut -d' ' -f1 | rev)"
 	tt=$((time ./lem-in < tmp.map) 2>&1 | tail -n 3 | grep real | rev | cut -f1 | rev)
 	obs=$(./lem-in -i < tmp.map | grep 'number of steps' | rev | cut -d' ' -f1 | rev)
