92370e8987cf25675c45f21cdaf2ec31da820df0
Goki LY <gly@student.42.fr>
2019-07-18 11:50:09 +0200

    Make the header file respect the norm.

--- incl/ft_lib_lem_in.h
+++ incl/ft_lib_lem_in.h
@@ -6,7 +6,7 @@
 /*   By: qtran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 11:24:47 by qtran             #+#    #+#             */
-/*   Updated: 2019/07/18 11:32:50 by gly              ###   ########.fr       */
+/*   Updated: 2019/07/18 11:49:45 by gly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,7 +124,6 @@ int						ft_parse_link(char *line, t_global *global,
 int						ft_db_room(t_global *g, char **tmp, t_input *input);
 int						ft_parse_room_tmp(char *line, t_global *global,
 		t_input *input);
-
 
 void					ft_print_solution(t_solution *sol);
 int						print_lem_in_simple(t_global *g);
